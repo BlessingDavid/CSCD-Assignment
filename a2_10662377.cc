f836d3e4feb26e44ce2d30b617d67280d6decbb3
BlessingDavid <42800923+BlessingDavid@users.noreply.github.com>
2018-08-30 01:20:00 +0000

    Create a2_10662377.cc

---
+++ a2_10662377.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+  using namespace std;
+  bool is_prme (int num {
+  int f_count = 0;
+  for (int i = 1; i <=num; i++) {
+  if (num % i == 0) {
+  f_ count++;
+      } 
+}
+if (f_count == 2)  {
+        return true;
+    } else {
+        return false:
+    }
+}
